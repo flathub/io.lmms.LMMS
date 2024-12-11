57cb6e14adac83f2b85df964b99375072313c78d
Hubert Figuière <hub@figuiere.net>
2024-12-10 20:02:51 -0500

    Misc cleanups and device permissions narrowing
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- lmms-wrap.sh
+++ lmms-wrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+export LD_LIBRARY_PATH=/app/lib:/app/lib/libgig
+export DSSI_PATH=/app/extensions/Plugins/dssi
+export VST_PATH=/app/extensions/Plugins/vst
+export VST3_PATH=/app/extensions/Plugins/vst3
+export LADSPA_PATH=/app/extensions/Plugins/ladspa:/app/lib/ladspa
 export LV2_PATH=$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2
 
 exec lmms.bin "$@"
